2ed86500545eb2333332e379d4b7fc5cb5a293c3
dahlia52 <101353482+dahlia52@users.noreply.github.com>
2022-10-22 00:23:30 +0900

    Add files via upload

--- Ch03_배열 구조체 포인터/pointer.c
+++ Ch03_배열 구조체 포인터/pointer.c
@@ -1,10 +1,16 @@
 # include <stdio.h>
 
 int main() {
-	int a = 10;
+	int x;
 	int* p;
-	p = &a;
-	printf("a = %d\n", *p); // *p는 a의 값
-	printf("%d \n", p); // p는 a의 주소
-	printf("%d \n", &a); // &a는 a의 주소
+	x = 10;
+	p = &x;
+	
+	printf("x=%d \n", x);
+	printf("*p=%d \n", *p);
+	*p = 20;
+	printf("x=%d \n", x);
+
+	*p = *p * 2;
+	printf("x=%d \n", x);
 }
