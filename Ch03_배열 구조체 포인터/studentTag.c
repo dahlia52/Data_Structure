3f5bdc51869a60a1b58d6733cc604c85e9fea7d5
dahlia52 <101353482+dahlia52@users.noreply.github.com>
2022-10-21 23:55:06 +0900

    Add files via upload

---
+++ Ch03_배열 구조체 포인터/studentTag.c
@@ -0,0 +1,16 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+struct studentTag {
+	char name[10];
+	int age;
+	double gpa;
+};
+void main() {
+	struct studentTag s1;
+	strcpy(s1.name, "kim");
+	s1.age = 20;
+	s1.gpa = 4.3;
+	printf("%s, %d, %.2f", s1.name, s1.age, s1.gpa);
+}
