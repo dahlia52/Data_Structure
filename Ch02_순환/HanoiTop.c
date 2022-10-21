95107d05e16d945933898ebff98d29aa8e99a47e
dahlia52 <101353482+dahlia52@users.noreply.github.com>
2022-10-21 23:18:14 +0900

    Add files via upload

---
+++ Ch02_순환/HanoiTop.c
@@ -0,0 +1,16 @@
+// 하노이탑
+# include <stdio.h>
+
+void hanoi_tower(int n, char from, char temp, char to) {
+	if (n == 1)
+		printf("원판1을 %c에서 %c으로 옮긴다.\n", from, to);
+	else {
+		hanoi_tower(n - 1, from, to, temp);
+		printf("원판 %d를 %c에서 %c으로 옮긴다.\n", n, from, to);
+		hanoi_tower(n - 1, temp, from, to);
+	}
+}
+int main(void) {
+	hanoi_tower(4, 'A', 'B', 'C');
+	return 0;
+}
